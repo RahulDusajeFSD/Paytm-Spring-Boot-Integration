f77c08ff8982cb15bfb5ed62b95d84e54ab6ad92
RahulDusajeFSD <dusaje.IBM@gmail.com>
2021-06-03 20:51:17 +0530

    paytm successful

---
+++ src/test/java/com/paytm/demo/SpringPaytmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paytm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaytmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
